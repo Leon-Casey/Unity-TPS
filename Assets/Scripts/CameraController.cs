8470f5c917bd1e694f9fda7e481b16d4b7307b2b
Leon Casey <43139614+Leon-Casey@users.noreply.github.com>
2021-06-17 19:10:00 +0200

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 1f;
+
+    public Transform crosshair;
+    public Camera cam;
+    public Transform muzzle;
+
+    //private CinemachineComposer composer;
+
+    private void Start()
+    {
+        //composer = GetComponent <CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineComposer>();
+    }
+
+    private void Update()
+    {
+
+
+        //float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        //float horizontal = Input.GetAxis("Mouse X") * sensitivity;
+
+        //Vector3 mousePos = Input.mousePosition;
+        //mousePos.z = transform.position.z - Camera.main.transform.position.z;
+        //transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
---
+++ Assets/Scripts/CharacterAiming.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CharacterAiming : MonoBehaviour
+{
+    //[SerializeField]
+    //private float sensitivity = 1f;
+    [SerializeField]
+    public float turnSpeed = 15f;
+
+    //private CinemachineVirtualCamera cinemachineCamera;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cinemachineCamera = GetComponent<CinemachineVirtualCamera>();
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        //float horizontal = Input.GetAxis("Mouse X") * sensitivity;
+
+        //cinemachineCamera.GetInputAxisProvider()
+
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+    }
+}
